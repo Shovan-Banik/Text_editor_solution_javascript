93335fb75c13b34814c5bd11884ec667ade6f12a
Shovan-Banik <shovan15-7790@diu.edu.bd>
2023-02-14 18:13:20 +0600

    text editor done

--- edit.js
+++ edit.js
@@ -1,16 +1,54 @@
-function inputValue(id){
-    const inputField=document.getElementById(id);
-    const inputFieldValue=inputField.value;
-    return inputFieldValue; 
-}
-function setValue(id,result){
-    document.getElementById(id).value=result;
-}
 document.getElementById('bold').addEventListener('click',function(){
-    const textValue=inputValue('text-area');
+    const textValue=document.getElementById('text-area');
     textValue.classList.add('font-bold');
-    // const boldIcon=document.getElementById('boldI');
-    // boldIcon.classList.add('mota');
-    // setValue('text-area',textValue);
-
+})
+document.getElementById('italic').addEventListener('click',function(){
+    const textValue=document.getElementById('text-area');
+    textValue.classList.add('italic');
+})
+document.getElementById('underline').addEventListener('click',function(){
+    const textValue=document.getElementById('text-area');
+    textValue.classList.add('underline');
+})
+document.getElementById('left').addEventListener('click',function(){
+    const textValue=document.getElementById('text-area');
+    textValue.classList.add('text-left');
+    textValue.classList.remove('text-center');
+    textValue.classList.remove('text-justify');
+    textValue.classList.remove('text-right');
+})
+document.getElementById('center').addEventListener('click',function(){
+    const textValue=document.getElementById('text-area');
+    textValue.classList.add('text-center');
+    textValue.classList.remove('text-left');
+    textValue.classList.remove('text-justify');
+    textValue.classList.remove('text-right');
+})
+document.getElementById('justify').addEventListener('click',function(){
+    const textValue=document.getElementById('text-area');
+    textValue.classList.add('text-justify');
+    textValue.classList.remove('text-left');
+    textValue.classList.remove('text-center');
+    textValue.classList.remove('text-right');
+})
+document.getElementById('right').addEventListener('click',function(){
+    const textValue=document.getElementById('text-area');
+    textValue.classList.add('text-right');
+    textValue.classList.remove('text-left');
+    textValue.classList.remove('text-justify');
+    textValue.classList.remove('text-center');
+})
+document.getElementById('upper').addEventListener('click',function(){
+    const textValue=document.getElementById('text-area');
+    textValue.classList.add('uppercase');
+})
+document.getElementById('size').addEventListener('click',function(event){
+    const textValue=document.getElementById('text-area');
+    const sizing=event.target.value;
+    textValue.style.fontSize=sizing+'px';
+})
+document.getElementById('colors').addEventListener('click',function(event){
+    const textValue=document.getElementById('text-area');
+    const colorValue=event.target.value;
+    textValue.style.color=colorValue;
 })
